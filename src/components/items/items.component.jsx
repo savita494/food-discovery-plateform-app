9a6f641ad05135e9b32c69e627e2b3486364cb8b
savita494 <60258608+savita494@users.noreply.github.com>
2021-03-07 09:06:32 +0530

    kk
    kk

--- src/components/items/items.component.jsx
+++ src/components/items/items.component.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import {Link} from 'react-router-dom';
+  import HotelPageHyderabad from '../../pages/hotelpagehyderabad/hotelpagehyderabad.components'
+//  import HotelPageHyderabad from '../../pages/hotelpagekochi/hotelpagekochi.components'
+//  import HotelPageHyderabad from '../../pages/hotelpagebangalore/hotelpagebangalore.components'
 
 import './items.styles.scss';
 
 const Items = ({ title, imageUrl, size, history, linkUrl, match }) => (
+ <div>
+
+<Link className='lnkClassList' to={"/hotelpagehyderabad.components"}>
+
   <div
     className={`${size} items`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
+  >  
+   
+
     <div
       className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
     />
+
     <div className='content'>
       
       <h1 className='title'>{title}</h1>
-      
       
 
       
     </div>
     
+
+    
   </div>
+  </Link>
+  </div>
+
   
 );
 
