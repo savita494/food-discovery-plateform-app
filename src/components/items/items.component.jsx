7ad55218686cf9653c8624ac70d5a54fb9a23f5a
savita494 <60258608+savita494@users.noreply.github.com>
2021-03-08 19:22:31 +0530

    kkkk
    kk

--- src/components/items/items.component.jsx
+++ src/components/items/items.component.jsx
@@ -1,48 +1,177 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import {Link} from 'react-router-dom';
-  import HotelPageHyderabad from '../../pages/hotelpagehyderabad/hotelpagehyderabad.components'
+  import HotelPageHyderabad from '../../pages/hotelpagehyderabad/hotelpagehyderabad.components';
   //import HotelPageHyderabad from '../../pages/hotels/hotels.components'
-//  import HotelPageHyderabad from '../../pages/hotelpagebangalore/hotelpagebangalore.components'
+  import Hotelpagebangalore from '../../pages/hotelpagebangalore/hotelpagebangalore.components';
+  import Hotelpagekochi from '../../pages/hotelpagekochi/hotelpagekochi.components';
 
 import './items.styles.scss';
 
-const Items = ({ title, imageUrl, size, history, linkUrl, match }) => (
- <div>
+class Item extends Component
+ {
+   //   constructor()
+//   {    super();
+// //imageUrl, size, history, linkUrl, match}
 
-<Link className='lnkClassList' to={"/hyderabad"}>
+//   }
+
+  render()
+  {      
+     
+     if(this.props.imageUrl=='https://static.toiimg.com/photo/71775173.cms')
+  {
+    return(
+      <div>
+        <Link className='lnkClassList' to={"/kochi"}>
+        <div
+        className={` items`}
+
+  >
+    <div       className='background-image'
+      style={{
+        backgroundImage: `url(${this.props.imageUrl})`     }}
+   /> 
+    <div className='content'>
+           <h1 className='title'>{this.props.title}</h1>
+          </div>
+          </div>
+</Link>
+  </div>
+    )
+  }
+  else if(this.props.imageUrl=='https://etimg.etb2bimg.com/photo/73411431.cms'){
+    return(
+      <div>
+        <Link className='lnkClassList' to={"/hyderabad"}>
+        <div
+        className={` items`}
+
+  >
+    <div       className='background-image'
+      style={{
+        backgroundImage: `url(${this.props.imageUrl})`     }}
+   /> 
+    <div className='content'>
+           <h1 className='title'>{this.props.title}</h1>
+          </div>
+          </div>
+</Link>
+  </div>
+    )
+
+  }
+  else{
+
+    return(
+      <div>
+        <Link className='lnkClassList' to={"/bangalore"}>
+        <div
+    className={` items`}
+  >
+    <div       className='background-image'
+      style={{
+        backgroundImage: `url(${this.props.imageUrl})`     }}
+   /> 
+    <div className='content'>
+           <h1 className='title'>{this.props.title}</h1>
+          </div>
+          </div>
+  
+</Link>
+  </div>
+    )
+  }
+
+    
+           
+  }
+
+}
+export default Item;
+
+
+// const  Items = ({ title, imageUrl, size, history, linkUrl, match }) => (
+//  <div>
+// <Link className='lnkClassList' to={"/hyderabad"}>
+ //   <div
+//     className={`${size} items`}
+//     onClick={() => history.push(`${match.url}${linkUrl}`)}
+//   >  
+//     <div
+//       className='background-image'
+//       style={{
+//         backgroundImage: `url(${imageUrl})`
+//       }}
+//     /> 
+//     <div className='content'>
+      //       <h1 className='title'>{title}</h1>
+      //     </div>
+    //   </div>
+//   </Link>
+//   <Link className='lnkClassList' to={"/hyderabad"}>
  
 
-  <div
-    className={`${size} items`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >  
+//   <div
+//     className={`${size} items`}
+//     onClick={() => history.push(`${match.url}${linkUrl}`)}
+//   >  
 
 
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    /> 
+//     <div
+//       className='background-image'
+//       style={{
+//         backgroundImage: `url(${imageUrl})`
+//       }}
+//     /> 
 
 
-    <div className='content'>
+//     <div className='content'>
       
-      <h1 className='title'>{title}</h1>
+//       <h1 className='title'>{title}</h1>
       
 
       
-    </div>
+//     </div>
     
 
     
-  </div>
-  </Link>
+//   </div>
+//   </Link>
+//   <Link className='lnkClassList' to={"/hyderabad"}>
+ 
 
-  </div>
+//  <div
+//    className={`${size} items`}
+//    onClick={() => history.push(`${match.url}${linkUrl}`)}
+//  >  
+
+
+//    <div
+//      className='background-image'
+//      style={{
+//        backgroundImage: `url(${imageUrl})`
+//      }}
+//    /> 
+
+
+//    <div className='content'>
+     
+//      <h1 className='title'>{title}</h1>
+     
+
+     
+//    </div>
+   
+
+   
+//  </div>
+//  </Link>
+
+
+//   </div>
 
   
-);
+// );
 
-export default withRouter(Items);
+// export default withRouter(Items);
